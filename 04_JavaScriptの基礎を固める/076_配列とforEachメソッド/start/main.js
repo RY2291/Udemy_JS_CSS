9366cf01a374cac90d024c72e8c65ba445d3a15e
RY2291 <prgr2498@gmail.com>
2021-12-07 14:39:37 +0900

    [fin]配列

---
+++ 04_JavaScriptの基礎を固める/076_配列とforEachメソッド/start/main.js
@@ -0,0 +1,6 @@
+const arry = [1,2,3,4,5];
+
+//  forEachは配列をループさせる
+arry.forEach(function(v, i, ary){
+  console.log(v, i, ary);
+})
--- 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
+++ 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
@@ -1,1 +1,21 @@
 const arry = [1,2,3,4,5];
+
+
+// reducaメソッド・・・・配列データの各要素を累積して1つの値にする
+
+arry.reduce(function(accu, curr){
+  console.log(accu, curr);
+  return accu + curr;
+}, 10);
+
+
+const str = "animation"
+const srtArry = str.split("");
+
+console.log(srtArry)
+
+const resulut = srtArry.reduce((accu, curr) => {
+  return accu + "<" + curr + ">";
+}, "")
+
+console.log(resulut);
